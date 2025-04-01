bfa6017c107e8043c06083dd77f672f980d8d537
Vojtěch Škopek <vojtba@gmail.com>
2025-04-01 21:38:49 +0200

    Implement getOccupancyHistory

--- backend/db/occupancy.js
+++ backend/db/occupancy.js
@@ -6,10 +6,29 @@ async function insertOccupancy(count){
             `INSERT INTO occupancy_log (people_count) VALUES ($1) RETURNING *`,
             [count]
         );
+        console.log("Succsfully added log to database.");
         return result.rows[0];
     }
     catch(error){
         console.error('Error when inserting log to database.');
         return null;
     }
+}
+
+async function getOccupancyHistory(limit = 50) {
+    try {
+      const result = await db.query(
+        `SELECT * FROM occupancy_log ORDER BY timestamp DESC LIMIT $1`,
+        [limit]
+      );
+      return result.rows;
+    } catch (error) {
+      console.error("Failed to fetch occupancy history:", error.message);
+      return [];
+    }
+}
+
+module.exports = {
+ insertOccupancy,
+ getOccupancyHistory
 }
