f77220944c972ed4945b90675340e9223a44ce6e
Vojtěch Škopek <vojtba@gmail.com>
2025-04-01 21:19:23 +0200

    implement exception handling

--- backend/db/client.js
+++ backend/db/client.js
@@ -2,12 +2,17 @@ const { Client } = require("pg");
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
 
-console.log(process.env.DATABASE_URL);
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
+try{
+      client.connect();
+      console.log('Succesfully connected to database.');
+}
+catch (error){
+    console.error('Error with connecting to database, ' + error);
+}
 
-client.connect();
 
 module.exports = client;
