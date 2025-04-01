1ec0b3c79a7a0ba70362b346c6bf3801eda526b8
Vojtěch Škopek <vojtba@gmail.com>
2025-04-01 21:04:56 +0200

    Create scrapeNTK.js

---
+++ backend/scrape/scrapeNTK.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+// const path = require("path");
+
+const LIBRARY_URL = "https://www.techlib.cz/cs/";
+
+//Function that fetches the number of people inside the library
+async function scrapeLibraryOccupancy() {
+try {
+    const { data: html } = await axios.get(LIBRARY_URL);
+
+    const $ = cheerio.load(html);
+
+    const spanText = $('div.panel-body.text-center.lead span').first().text().trim();
+    const occupancy = parseInt(spanText, 10);
+
+    if (isNaN(occupancy)) {
+        console.error("Could not parse occupancy count.");
+        return null;
+    } else {
+        console.log(`Current NTK occupancy: ${occupancy} people`);
+        return occupancy;
+    }
+} catch (err) {
+    console.error("Failed to fetch NTK data:", err.message);
+    return null;
+}
+}
+module.exports = scrapeLibraryOccupancy;
+// scrapeLibraryOccupancy();
