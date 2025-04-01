34a9646f62f424491f9ef72fa2f57e81fb0a0853
Vojtěch Škopek <vojtba@gmail.com>
2025-04-01 23:13:38 +0200

    Added date selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
+import './App.css';
 import axios from 'axios';
 import * as recharts from 'recharts';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); 
 
   useEffect(() => {
+    if (!selectedDate) return;
+
     axios.post('/api/graphql', {
       query: `
         query {
-          occupancyHistory(limit: 5) {
-            id
-            timestamp
-            people_count
+          dailyAverage(date: "${selectedDate}") {
+            interval_start
+            average_count
           }
         }
       `
@@ -25,30 +27,39 @@ function App() {
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-  }, []);
+  }, [selectedDate]);
 
   return (
     <>
-      <p>{data ? JSON.stringify(data) : 'Loading...'}</p>
+      <div>
+        <label htmlFor="date-selector">Select Date: </label>
+        <input
+          id="date-selector"
+          type="date"
+          value={selectedDate}
+          onChange={(e) => setSelectedDate(e.target.value)}
+        />
+      </div>
 
-      {data && data.data && data.data.occupancyHistory && (
+      {data && data.data && data.data.dailyAverage && (
         <recharts.LineChart
           width={500}
           height={300}
-          data={data.data.occupancyHistory.map(item => ({
-                timestamp: new Date(item.timestamp).toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' }),
-                people_count: item.people_count
-                }))}>
-                  
-          <recharts.Line type="monotone" dataKey="people_count" stroke="#8884d8" />
+          data={data.data.dailyAverage.map(item => ({
+            timestamp: new Date(item.interval_start).toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' }),
+            average_count: item.average_count
+          }))}>
+
+          <recharts.Line type="monotone" dataKey="average_count" stroke="#8884d8" />
           <recharts.CartesianGrid stroke="#ccc" />
           <recharts.XAxis dataKey="timestamp" />
           <recharts.YAxis />
+          <recharts.Tooltip/>
         </recharts.LineChart>
-
-        
       )}
 
+      {!data && <p>Loading...</p>}
+      {data && data.data && data.data.dailyAverage.length === 0 && <p>No data available for the selected date.</p>}
     </>
   );
 }
