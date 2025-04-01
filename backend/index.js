e25acb796af966fa7ed8a4da9231b7f10739f022
Vojtěch Škopek <vojtba@gmail.com>
2025-04-01 23:02:31 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const scrapeLibraryOccupancy = require('./scrape/scrapeNTK');
 const { graphqlHTTP } = require("express-graphql");
-const { getOccupancyHistory, insertOccupancy } = require("./db/occupancy");
+const { getOccupancyHistory, insertOccupancy, dailyAverage } = require("./db/occupancy");
 const schema = require("./graphql/schema");
 const express= require('express');
 const app = express();
@@ -14,6 +14,7 @@ router.post("/graphql", graphqlHTTP({
     schema,
     rootValue: {
       occupancyHistory: async ({ limit }) => await getOccupancyHistory(limit || 50),
+      dailyAverage: async ({ date }) => await dailyAverage(date),
     },
     graphiql: true,
 }));
