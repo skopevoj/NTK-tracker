a1a3c43b2a386d7cf2b901cee7d793cbe23ac3a5
Vojtěch Škopek <vojtba@gmail.com>
2025-04-01 21:38:38 +0200

    Implement grphql route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
 const scrapeLibraryOccupancy = require('./scrape/scrapeNTK');
+const { graphqlHTTP } = require("express-graphql");
+const { getOccupancyHistory, insertOccupancy } = require("./db/occupancy");
+const schema = require("./graphql/schema");
+const express= require('express');
+const app = express();
+require('dotenv').config();
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    rootValue: {
+      occupancyHistory: async ({ limit }) => await getOccupancyHistory(limit || 50),
+    },
+    graphiql: true,
+}));
+
 
 (async () => {
     const occupancy = 500; //await scrapeLibraryOccupancy();
     console.log(`Library occupancy fetched: ${occupancy}`);
 })();
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
