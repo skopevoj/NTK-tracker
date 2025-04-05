588832b8c427bac039304dd575d8c207324fc66c
Vojtěch Škopek <39000132+WarriorKnight@users.noreply.github.com>
2025-04-05 18:34:32 +0200

    formatted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import DataView from './components/DataView.jsx'
 function App() {
   return (
     <>
-    <h1>NTK Tracker</h1>
-    <p>This app allows users to track and visualize data over time using interactive charts. Select a date to view specific data points.</p>
+      <h1>NTK Tracker</h1>
+      <p>This app allows users to track and visualize data over time using interactive charts. Select a date to view specific data points.</p>
       <DataView/>
     </>
   );
