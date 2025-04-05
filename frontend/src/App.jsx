067b127a17e9728490f83f0129e3143512a7449e
Vojtěch Škopek <39000132+WarriorKnight@users.noreply.github.com>
2025-04-05 18:31:24 +0200

    Added short description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,11 @@
 import './App.css';
-import axios from 'axios';
-import * as recharts from 'recharts';
-import { useState, useEffect } from 'react';
 import DataView from './components/DataView.jsx'
 function App() {
-  const [data, setData] = useState(null);
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); 
-
-  // useEffect(() => {
-  //   if (!selectedDate) return;
-
-  //   axios.post('/api/graphql', {
-  //     query: `
-  //       query {
-  //         dailyAverage(date: "${selectedDate}") {
-  //           interval_start
-  //           average_count
-  //         }
-  //       }
-  //     `
-  //   })
-  //     .then(response => {
-  //       setData(response.data);
-  //       console.log('API Response:', response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching data:', error);
-  //     });
-  // }, [selectedDate]);
-
   return (
     <>
-    <DataView/>
-      {/* <div>
-        <label htmlFor="date-selector">Select Date: </label>
-        <input
-          id="date-selector"
-          type="date"
-          value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-        />
-      </div>
-
-      {data && data.data && data.data.dailyAverage && (
-        <recharts.LineChart
-          width={500}
-          height={300}
-          data={data.data.dailyAverage.map(item => ({
-            timestamp: new Date(item.interval_start).toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' }),
-            average_count: item.average_count
-          }))}>
-
-          <recharts.Line type="monotone" dataKey="average_count" stroke="#8884d8" />
-          <recharts.CartesianGrid stroke="#ccc" />
-          <recharts.XAxis dataKey="timestamp" />
-          <recharts.YAxis />
-          <recharts.Tooltip/>
-        </recharts.LineChart>
-      )}
-
-      {!data && <p>Loading...</p>}
-      {data && data.data && data.data.dailyAverage.length === 0 && <p>No data available for the selected date.</p>} */}
+    <h1>NTK Tracker</h1>
+    <p>This app allows users to track and visualize data over time using interactive charts. Select a date to view specific data points.</p>
+      <DataView/>
     </>
   );
 }
