87e44ecd1361ae37f7fc23ba1c4f5307a3e5b1e4
Vojtěch Škopek <39000132+skopevoj@users.noreply.github.com>
2025-10-21 14:37:50 +0200

    fix: remove .env logging

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,8 +1,6 @@
 const { Client } = require("pg");
 require("dotenv").config({ path: require("path").resolve(__dirname, "../../.env") });
 
-console.log("Connecting to database..." + process.env.DATABASE_URL);
-
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
